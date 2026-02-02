098856c75f1d56bc1c577bccf6f8009d6df5cb8b
mohashish28 <143516792+mohashish28@users.noreply.github.com>
2026-02-02 11:20:00 +0200

    Update veh.js

--- veh.js
+++ veh.js
@@ -1,88 +1,73 @@
 class vehicle {
-constructor (x,y){
-this.pos = createVector(x,y)
-this.target = createVector(x,y)
-this.vel = createVector(0,0)
-this.acc = createVector(0,0)
-this.maxspeed =10
-this.maxforce = .5
+  constructor(x, y) {
+    this.pos = createVector(x, y)
+    this.target = createVector(x, y)
+    this.vel = p5.Vector.random2D()
+    this.acc = createVector(0, 0)
 
-}
+    this.maxspeed = 10
+    this.maxforce = 0.15   // 🔹 lower = smoother curves
+  }
 
-show (){
+  show() {
     strokeWeight(1)
-    fill(255)
-    point(this.pos.x,this.pos.y)
-  ///  circle(this.pos.x,this.pos.y,20)
-}
+    stroke(255)
+    point(this.pos.x, this.pos.y)
+  }
 
-update(){
-this.vel.add(this.acc)
-this.pos.add (this.vel)
-this.acc.set(0,0)
+  update() {
+    // apply forces
+    this.vel.add(this.acc)
 
+    // 🔹 SMOOTHING (Bezier-like curvature)
+    let desired = this.vel.copy().setMag(this.maxspeed)
+    this.vel.lerp(desired, 0.05)   // ← magic line
 
-}
+    this.pos.add(this.vel)
+    this.acc.set(0, 0)
+  }
 
-applyforce(f){
-this.acc.add(f)
-}
+  applyforce(f) {
+    this.acc.add(f)
+  }
 
-flee(target){
-let force = p5.Vector.sub(this.pos,target)
-let d = force.mag()
-let speed =this.maxspeed
-if (d<60){
-
-    force.setMag(speed)
-    force.sub(this.vel)
-    force.mult(-1)
-force.mult(this.maxforce)
-}else {
-
-force.set(0,0)
-
-}
-
-
-
-return force
-}
-
-arrive (target){
-
-    let force = p5.Vector.sub(target,this.pos)
+  flee(target) {
+    let force = p5.Vector.sub(this.pos, target)
     let d = force.mag()
-    let speed =this.maxspeed
-    if (d<60){
-    
-        speed = map (d,0,60,0,this.maxspeed)
-  
+
+    if (d < 60) {
+      force.setMag(this.maxspeed)
+      force.sub(this.vel)
+      force.limit(this.maxforce)
+      force.mult(-1)
+      return force
     }
+
+    return createVector(0, 0)
+  }
+
+  arrive(target) {
+    let force = p5.Vector.sub(target, this.pos)
+    let d = force.mag()
+
+    let speed = this.maxspeed
+    if (d < 60) {
+      speed = map(d, 0, 60, 0, this.maxspeed)
+    }
+
     force.setMag(speed)
     force.sub(this.vel)
-    force.mult(this.maxforce)
-
+    force.limit(this.maxforce)
     return force
-}
+  }
 
+  behaviour() {
+    let mouse = createVector(mouseX, mouseY)
 
+    let fleeForce = this.flee(mouse)
+    let arriveForce = this.arrive(this.target)
 
-behaviour(){
-
-let flee 
-let mous = createVector(mouseX,mouseY)
-flee =this.flee(mous)
-this.applyforce(flee)
-
-
-let arrive 
-arrive = this.arrive(this.target)
-this.applyforce(arrive)
-
-}
-
-
-
-
+    this.applyforce(fleeForce)
+    this.applyforce(arriveForce)
+  }
 }
